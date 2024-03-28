adcb0514d05e19f38aab7ccfb003ef53ff2cdce4
EcclesN <Niall.Eccles@hmhco.com>
2024-03-28 21:01:51 +0000

    Create grid

--- src/app/components/editor.tsx
+++ src/app/components/editor.tsx
@@ -1,18 +1,19 @@
 import Markdown from 'react-markdown';
 import { useState } from 'react';
 import { Textarea } from '@mantine/core';
+import { SimpleGrid } from '@mantine/core';
 
 export function Editor() {
   const [markdown, setMarkdown] = useState('# Test');
 
   return (
-    <div>
+    <SimpleGrid cols={2}>
       <Textarea
-        value={markdown}
-        onChange={(event) => setMarkdown(event.currentTarget.value)}
-      />
+          value={markdown}
+          onChange={(event) => setMarkdown(event.currentTarget.value)}
+        />
       <Markdown>{markdown}</Markdown>
-    </div>
+    </SimpleGrid>
   );
 }
 
