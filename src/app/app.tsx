257c1876b0a145f243806543689e2c824a8c3554
EcclesN <Niall.Eccles@hmhco.com>
2024-03-29 16:41:08 +0000

    Update AppShell

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,35 +1,36 @@
 import Editor from './components/editor';
 import '@mantine/core/styles.css';
-import { MantineProvider, AppShell, Burger } from '@mantine/core';
+import { MantineProvider, AppShell, Burger, Group, Skeleton } from '@mantine/core';
 import { useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
 
 
 export function App() {
-  const [opened, toggle] = useState(false);
+  const [opened, { toggle }] = useDisclosure();
+
   return (
     <MantineProvider>
       <AppShell
         header={{ height: 60 }}
-        navbar={{
-          width: 300,
-          breakpoint: 'sm',
-          collapsed: { mobile: !opened },
-        }}
+        navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
         padding="md"
       >
         <AppShell.Header>
-          <Burger
-            opened={opened}
-            onClick={toggle}
-            hiddenFrom="sm"
-            size="sm"
-          />
-          <div>Finch</div>
+          <Group h="100%" px="md">
+            <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          </Group>
         </AppShell.Header>
-
-        <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
-
-        <AppShell.Main><Editor/></AppShell.Main>
+        <AppShell.Navbar p="md">
+          Navbar
+          {Array(15)
+            .fill(0)
+            .map((_, index) => (
+              <Skeleton key={index} h={28} mt="sm" animate={false} />
+            ))}
+        </AppShell.Navbar>
+        <AppShell.Main>
+          <Editor/>
+        </AppShell.Main>
       </AppShell>
     </MantineProvider>
   );
