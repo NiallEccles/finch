f49f488215f65810551ac9f7e2eee486ebeacbaf
EcclesN <Niall.Eccles@hmhco.com>
2024-03-29 16:31:09 +0000

    Add AppShell

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,36 @@
 import Editor from './components/editor';
 import '@mantine/core/styles.css';
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, AppShell, Burger } from '@mantine/core';
+import { useState } from 'react';
 
 
 export function App() {
+  const [opened, toggle] = useState(false);
   return (
     <MantineProvider>
-      <Editor/>
+      <AppShell
+        header={{ height: 60 }}
+        navbar={{
+          width: 300,
+          breakpoint: 'sm',
+          collapsed: { mobile: !opened },
+        }}
+        padding="md"
+      >
+        <AppShell.Header>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <div>Finch</div>
+        </AppShell.Header>
+
+        <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+
+        <AppShell.Main><Editor/></AppShell.Main>
+      </AppShell>
     </MantineProvider>
   );
 }
