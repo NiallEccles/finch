ed63061e3d98d85ae90a3fc8a06988254c7ca5f8
EcclesN <Niall.Eccles@hmhco.com>
2024-03-30 15:40:24 +0000

    Display directory files in navbar

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from 'react';
 import Editor from './components/editor';
 import '@mantine/core/styles.css';
-import { MantineProvider, AppShell, Burger, Group, Skeleton, Button } from '@mantine/core';
+import { MantineProvider, AppShell, Burger, Group, Skeleton, Button, NavLink } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import exampleMarkdown from '../exampleMarkdown';
+
+type Files = {key: string, value: FileSystemFileHandle | FileSystemDirectoryHandle};
 
 
 export function App() {
   const [opened, { toggle }] = useDisclosure();
   const [markdown, setMarkdown] = useState<string|undefined>(exampleMarkdown);
   const [handle, setHandle] = useState<FileSystemFileHandle>();
+  const [files, setFiles] = useState<Files[]>([]);
 
   const handleClick = async () => {
     const [fileHandle] = await window.showOpenFilePicker();
@@ -38,9 +41,12 @@ export function App() {
   const handleDirectoryPicker = () => {
     const showDirectoryPicker = async () => {
       const handle = await window.showDirectoryPicker();
+      const filesInDirectory: Files[] = [];
       for await (const [key, value] of handle.entries()) {
         console.log({ key, value });
+        filesInDirectory.push({key, value});
       }
+      setFiles(filesInDirectory);
       return handle;
     }
 
@@ -75,11 +81,26 @@ export function App() {
           <Button mb={4} variant="default" onClick={handleDirectoryPicker}>Select Directory</Button>
           <Button mb={4} variant="default" onClick={handleSave}>Create File</Button>
           <Button variant="default" onClick={handleClick}>Open File</Button>
-          {Array(15)
-            .fill(0)
-            .map((_, index) => (
-              <Skeleton key={index} h={28} mt="sm" animate={false} />
-            ))}
+          {/*{Array(15)*/}
+          {/*  .fill(0)*/}
+          {/*  .map((_, index) => (*/}
+          {/*    <Skeleton key={index} h={28} mt="sm" animate={false} />*/}
+          {/*  ))}*/}
+
+          {
+            files.map((item: Files, index: number) => (
+              <NavLink
+                href="#required-for-focus"
+                key={item.value.name}
+                // active={index === active}
+                label={item.value.name}
+                description={item.value.name}
+                // rightSection={item.}
+                // leftSection={<item.icon size="1rem" stroke={1.5} />}
+                // onClick={() => setActive(index)}
+              />
+            ))
+          }
         </AppShell.Navbar>
         <AppShell.Main>
           <Editor markdown={markdown} setMarkdown={setMarkdown} />
